1ca671e53a6977b13670bfcdc50e867d950824ff
EsinShadrach <esinshadrachrafe@gmail.com>
2025-03-05 02:39:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# swift_drop
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/rafeesin/swift_drop/swift_drop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.rafeesin.swift_drop.swift_drop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
